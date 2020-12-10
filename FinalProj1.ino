ac2716103e448367ada33c9323f50a45e0120931
rockannie <69537618+rockannie@users.noreply.github.com>
2020-12-09 18:11:20 -0700

    Add files via upload

--- FinalProj1.ino
+++ FinalProj1.ino
@@ -2,7 +2,13 @@
 
 //define the song notes in an array in order, my ex. is the start of 'Mary had a little lamb'
 //NOTE: replacing all #'s with proper wiring, they are placeholders 
-int notes[] = {NOTE_E3, NOTE_D3, NOTE_C3, NOTE_D3, NOTE_E3, NOTE_E3, NOTE_E3};
+int notes[] = {NOTE_C3, NOTE_C3, NOTE_D3, NOTE_D3, NOTE_E3, NOTE_G3, NOTE_E3, NOTE_C3, NOTE_C3, NOTE_C3, NOTE_D3, 
+NOTE_D3, NOTE_E3, NOTE_C3, NOTE_C3, NOTE_C3, NOTE_D3, NOTE_D3, NOTE_E3, NOTE_G3, NOTE_E3, NOTE_C3, NOTE_A3, 
+NOTE_D3, NOTE_F3, NOTE_E3, NOTE_C3, NOTE_C4, NOTE_C4, NOTE_A3, NOTE_C4, NOTE_B3, NOTE_B3, NOTE_G3, NOTE_C4, 
+NOTE_C4, NOTE_A3, NOTE_C4, NOTE_B3, NOTE_G3, NOTE_F3, NOTE_E3, NOTE_F3, NOTE_G3, NOTE_A3, NOTE_B3, NOTE_C4,
+NOTE_A3, NOTE_D3, NOTE_E3, NOTE_C3};
+
+int counter = 0;
 
 //define buzzer
 const int buzzerPin = 11;
@@ -16,73 +22,65 @@ const int latchPin = 3;
 byte leds = 0;
 
 //define several different IDs for inputs/outputs, replace #'s with correct wiring
-const int buttonPin1 = 5;
-const int buttonPin2 = 6;
-const int buttonPin3 = 7;
-const int buttonPin4 = 8;
-const int buttonPin5 = 9;
-const int buttonPin6 = 10;
-const int buttonPin7 = 12;
-const int buttonPin8 = 13;
+const int buttonPin1 = 5; //HIGHC 8
+const int buttonPin2 = 6; //B   7
+const int buttonPin3 = 7; //A   6
+const int buttonPin4 = 8; //G   5
+const int buttonPin5 = 9; //F   4
+const int buttonPin6 = 10; //E  3
+const int buttonPin7 = 12; //D  2
+const int buttonPin8 = 13; //C  1
+
 
 //sort buttons into array
-int buttons[] = {buttonPin8, buttonPin7, buttonPin6, buttonPin5, buttonPin4, buttonPin3, buttonPin2, buttonPin1};
+int buttons[] = {buttonPin8, buttonPin8, buttonPin7, buttonPin7, buttonPin6, buttonPin4, buttonPin6, buttonPin8,
+buttonPin8, buttonPin8, buttonPin7, buttonPin7, buttonPin6, buttonPin8, buttonPin8, buttonPin8, buttonPin7, 
+buttonPin7, buttonPin6, buttonPin4, buttonPin6, buttonPin8, buttonPin3, buttonPin7, buttonPin5, buttonPin6, 
+buttonPin8, buttonPin1, buttonPin1, buttonPin3, buttonPin1, buttonPin2, buttonPin2, buttonPin4, buttonPin1, 
+buttonPin1, buttonPin3, buttonPin1, buttonPin2, buttonPin4, buttonPin5, buttonPin6, buttonPin5, buttonPin4, 
+buttonPin3, buttonPin2, buttonPin1, buttonPin3, buttonPin7, buttonPin6, buttonPin8};
 
-//define LEDS
-//const int ledPin1 = #;
-//const int ledPin2 = #;
-//const int ledPin3 = #;
-//const int ledPin4 = #;
-//const int ledPin5 = #;
-//const int ledPin6 = #;
-//const int ledPin7 = #;
-//const int ledPin8 = #;
+int ledvalues[] = {0, 0, 1, 1, 2, 4, 2, 0, 0, 0, 1, 1, 2, 0, 0, 0, 1, 1, 2, 4, 2, 0, 5, 1, 3, 2, 0, 7, 7, 5,
+7, 6, 6, 4, 7, 7, 5, 7, 6, 4, 3, 2, 3, 4, 5, 6, 7, 5, 1, 2, 0};
 
-//sort leds into array
-//int leds[] = {ledPin1, ledPin2, ledPin3, ledPin4, ledPin5, ledPin6, ledPin7, ledPin8};
 
 void setup() {
   Serial.begin(9600);
   //set up buttons
-  for (int i = 0; i < sizeof(buttons)-1; i++)
-  {
-      pinMode(buttons[i], INPUT);
-      //pinMode(leds[i], OUTPUT);
-  }
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+  pinMode(buttonPin3, INPUT_PULLUP);
+  pinMode(buttonPin4, INPUT_PULLUP);
+  pinMode(buttonPin5, INPUT_PULLUP);
+  pinMode(buttonPin6, INPUT_PULLUP);
+  pinMode(buttonPin7, INPUT_PULLUP);
+  pinMode(buttonPin8, INPUT_PULLUP);
   //setup shift register
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
 }
 
+
 void loop() {
-  int counter = 0;
-  updateShiftRegister();
-  while (counter < sizeof(notes)-1)
+  //updateShiftRegister();
+  while (counter < 50)
   {
-      bitSet(leds, counter);
+      //Serial.println(counter);
+      bitSet(leds, ledvalues[counter]);
       updateShiftRegister();
-      //int currentLED = leds[counter];
-      //digitalWrite(currentLED, HIGH);
       int currentButton = buttons[counter];
       int buttonPressed = digitalRead(currentButton);
-      if(buttonPressed == 1)
+      if(buttonPressed == 0)
       {
-          tone(buzzerPin, notes[0], 1000);
-          bitClear(leds, counter);
+          //Serial.println(buttonPressed);
+          tone(buzzerPin, notes[counter], 1000);
+          bitClear(leds, ledvalues[counter]);
           updateShiftRegister();
-          //digitalWrite(currentLED, LOW);
           counter++;
-      }else{
-          bitClear(leds, counter);
-          updateShiftRegister();
-          bitSet(leds, counter);
-          updateShiftRegister();
-          //digitalWrite(currentLED, LOW);
-          //digitalWrite(currentLED, HIGH);
       }
-  }
-}
+   }
+} 
 
 void updateShiftRegister()
 {
